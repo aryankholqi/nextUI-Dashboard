9b4e45044fb059fb118c6697dfe2787c53d284fd
mohammadNosratii <mohammadnosratii2002@gmail.com>
2024-06-29 00:12:34 +0330

    dev login page

--- src/components/Templates/Login/LoginForm.tsx
+++ src/components/Templates/Login/LoginForm.tsx
@@ -1,9 +1,9 @@
 export default function LoginForm() {
-    return (
-        <div className="bg-red-400 p-5 flex flex-col gap-4 items-center absolute">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Name" />
-        </div>
-    )
+  return (
+    <div className="bg-lime-900 rounded-3xl child:rounded-md child:ps-1 w-[35%] h-[80%] p-5 flex flex-col justify-center gap-4 items-center">
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Name" />
+    </div>
+  );
 }
