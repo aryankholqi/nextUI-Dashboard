0569a342af88d22782ffdb7b8533069dc17fe784
Aryan kholqi <aryankholqi@gmail.com>
2024-07-03 14:40:43 +0330

    few changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,11 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import DashboardLayout from "../layouts/DashboardLayout";
 import AuthLayout from "../layouts/AuthLayout";
 
-const authRoutes: string[] = ["/login", "/register"];
+const authRoutes: string[] = ["/login"];
 
 export const Route = createRootRoute({
     component: () => {
-        const pathname = window.location.pathname;
+        const { pathname } = window.location;
         const Layout = authRoutes.includes(pathname) ? AuthLayout : DashboardLayout;
 
         return (
