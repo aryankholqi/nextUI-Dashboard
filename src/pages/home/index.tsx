715e695560743cb8434567f4f5b8ac1778ae787f
Aryan kholqi <aryankholqi@gmail.com>
2024-07-10 20:34:50 +0330

    few changes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,6 @@
-import MainTable from "../../components/Modules/Table/MainTable";
-
 export default function Home() {
     return (
-        <div className="mt-8">
-            <MainTable />
+        <div>
         </div>
     )
 }
