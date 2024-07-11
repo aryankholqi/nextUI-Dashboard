60dccea0cd115159bb8c250feb5a14c0d30ee7a6
Aryan kholqi <aryankholqi@gmail.com>
2024-07-11 11:51:19 +0330

    fix vercel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,17 @@
 import AreaChartBox from "../../components/Templates/Home/AreaChartBox";
-import BarChartBox from "../../components/Templates/Home/BarChartBox";
 import TotalBalance from "../../components/Templates/Home/TotalBalance";
 import YourCards from "../../components/Templates/Home/YourCards";
 
 export default function Home() {
   return (
     <div className="flex gap-5 mt-5">
-      {/* <div className="grid grid-cols-2 gap-5 w-[72%]">
+      <div className="grid grid-cols-2 gap-5 w-[72%]">
         <TotalBalance />
         <AreaChartBox />
       </div>
       <div className="w-[28%]">
         <YourCards />
-      </div> */}
-      <BarChartBox />
+      </div>
     </div>
   );
 }
