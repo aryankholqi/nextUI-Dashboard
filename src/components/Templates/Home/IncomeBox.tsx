392e82654be0c1a76650fa2f7dff0d20c51889ea
Aryan kholqi <aryankholqi@gmail.com>
2024-07-11 13:22:07 +0330

    fix income circular propgress color

--- src/components/Templates/Home/IncomeBox.tsx
+++ src/components/Templates/Home/IncomeBox.tsx
@@ -1,13 +1,17 @@
 import { Card, CircularProgress } from '@nextui-org/react'
 import formatPrice from '../../../utils/formatPrice'
 import { incomeProps } from '../../../interfaces/income.interface'
+import { useTheme } from 'next-themes'
 
-export default function IncomeBox({ price, Icon, iconBgColor, indicatorBgColor, percentage }: incomeProps) {
+export default function IncomeBox({ id, price, Icon, iconBgColor, percentage }: incomeProps) {
+    const { theme } = useTheme()
+
+    const iconbg = theme === "light" ? iconBgColor.light : iconBgColor.dark
     return (
         <Card className='p-6 bg-white dark:bg-black dark:text-white rounded-4xl shadow-none space-y-4 flex flex-col justify-between'>
             <div className='flex items-center justify-between gap-10'>
                 <div className='space-y-4 mb-8'>
-                    <span className={`bg-[${iconBgColor?.light}] dark:bg-[${iconBgColor?.dark}] p-2 rounded-full flex items-center justify-center w-max`}>
+                    <span style={{ backgroundColor: iconbg }} className={`p-2 rounded-full flex items-center justify-center w-max`}>
                         <Icon />
                     </span>
                     <p className='font-poppinsMedium text-2xl'>Income</p>
@@ -15,7 +19,7 @@ export default function IncomeBox({ price, Icon, iconBgColor, indicatorBgColor, 
                 <CircularProgress
                     classNames={{
                         svg: "w-36 h-36",
-                        indicator: `stroke-[${indicatorBgColor?.light}] dark:stroke-[${indicatorBgColor?.dark}]`,
+                        indicator: `${id === 1 ? "stroke-[#00FF6B] dark:stroke-[#1AFF79]" : "stroke-[#FF6756] dark:stroke-[#FF7E70]"}`,
                         track: "stroke-[#F3F4F7] dark:stroke-[#373737]",
                         value: "text-lg font-poppinsRegular text-primaryGray dark:text-[#B6B6B6]",
                     }}
